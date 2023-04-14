c88caf6649c2bd4fa3aa1fa285ce5280ec8d5df9
Abror <abroryaxshiboyev2002@gmail.com>
2023-04-14 18:22:37 +0500

    UserResource

--- app/Http/Resources/User/UserResource.php
+++ app/Http/Resources/User/UserResource.php
@@ -18,7 +18,7 @@ class UserResource extends JsonResource
             'id'=>$this->id,
             'name'=>$this->name,
             'phone'=>$this->phone,
-            'image'=>storage_path().'/images/'.$this->photo->file ?? null,
+            'image'=>$this->photo->file ?? null,
             'role'=>$this->roles[0]->name
         ];
     }
