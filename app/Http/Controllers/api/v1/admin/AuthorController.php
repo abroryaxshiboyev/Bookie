a785ab56b76f478569b0236903efc678a49911f6
Abror <abroryaxshiboyev2002@gmail.com>
2023-05-26 17:02:11 +0500

    author o'zgertildi

--- app/Http/Controllers/api/v1/admin/AuthorController.php
+++ app/Http/Controllers/api/v1/admin/AuthorController.php
@@ -49,7 +49,7 @@ class AuthorController extends Controller
             return response([
                 'message'=>'one Author',
                 'data'=>new OneAuthorResource($author),
-                'total'=>$count->total()
+                'total'=>$count->count()
             ]);
         }
         else {
