a77e144ea53d92bf24d25b558bf309909e937d9f
abroryaxshiboyev <110038864+abroryaxshiboyev@users.noreply.github.com>
2023-06-22 17:17:09 +0500

    book audios

--- app/Http/Resources/Book/AdminOneBookResource.php
+++ app/Http/Resources/Book/AdminOneBookResource.php
@@ -25,7 +25,7 @@ class AdminOneBookResource extends JsonResource
             'price'=>$this->price,
             'categories'=>BookCategoryResource::collection($this->categories),
             'image'=>!empty($this->photo->file) ? env('APP_URL')."/images/".$this->photo->file:null,
-            'audios'=>!empty($this->audio->first()->url) ? BookAudioResource::collection($this->audio):[],
+            'audios'=>!empty($this->audio->first()) ? BookAudioResource::collection($this->audio):[],
             'rating'=>$this->rating,
         ];
     }
