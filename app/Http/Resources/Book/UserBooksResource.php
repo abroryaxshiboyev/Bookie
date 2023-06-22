af63d33ad4d21f181852476ad45b926b14dade3f
abroryaxshiboyev <110038864+abroryaxshiboyev@users.noreply.github.com>
2023-06-22 16:53:27 +0500

    book audios

--- app/Http/Resources/Book/UserBooksResource.php
+++ app/Http/Resources/Book/UserBooksResource.php
@@ -25,7 +25,7 @@ class UserBooksResource extends JsonResource
             'price'=>$this->price,
             'categories'=>BookCategoryResource::collection($this->categories),
             'image'=>!empty($this->photo->file) ? env('APP_URL')."/images/".$this->photo->file:null,
-            'audios'=>[!empty($this->audios->first()->url) ? env('APP_URL')."/audios/".$this->audios->first()->url:null],
+            'audios'=>[!empty($this->audios->first()->url) ? new BookAudioResource($this->audios->first()):[]],
             'rating'=>$this->rating,
             'baskets'=>count($this->basket),
             'favorite'=>count($this->favorite)
