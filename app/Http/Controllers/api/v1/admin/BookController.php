dafbde48f94b74ea033bef871817f4dd2cbe84e6
abroryaxshiboyev <110038864+abroryaxshiboyev@users.noreply.github.com>
2023-07-22 14:17:10 +0500

    book update image

--- app/Http/Controllers/api/v1/admin/BookController.php
+++ app/Http/Controllers/api/v1/admin/BookController.php
@@ -18,92 +18,99 @@ use Illuminate\Support\Facades\Storage;
 
 class BookController extends Controller
 {
-    public function booksearch(Request $request){
-        $books=Book::search($request->input('search'))->paginate($request->input('limit'));
+    public function booksearch(Request $request)
+    {
+        $books = Book::search($request->input('search'))->paginate($request->input('limit'));
         return response([
-            'message'=>"all search books",
-            'data'=>[
-                'books'=>UserBooksResource::collection($books),
-                'total'=>$books->total(),
-                ]
+            'message' => "all search books",
+            'data' => [
+                'books' => UserBooksResource::collection($books),
+                'total' => $books->total(),
+            ]
         ]);
     }
-    public function recommendation(Request $request){
+    public function recommendation(Request $request)
+    {
 
-        $book=Book::orderBy('rating','desc')->paginate($request->input('limit'));
+        $book = Book::orderBy('rating', 'desc')->paginate($request->input('limit'));
         return response([
-            'message'=>"all recommendation books",
-            'data'=>UserBooksResource::collection($book),
-            'total'=>$book->total()
+            'message' => "all recommendation books",
+            'data' => UserBooksResource::collection($book),
+            'total' => $book->total()
         ]);
     }
 
-    public function popular(Request $request){
-        $book=Book::orderBy('click','desc')->paginate($request->input('limit'));
+    public function popular(Request $request)
+    {
+        $book = Book::orderBy('click', 'desc')->paginate($request->input('limit'));
         return response([
-            'message'=>"all popular books",
-            'data'=>UserBooksResource::collection($book),
-            'total'=>$book->total()
+            'message' => "all popular books",
+            'data' => UserBooksResource::collection($book),
+            'total' => $book->total()
         ]);
     }
-    public function newbooks(Request $request){
-        $book=Book::orderBy('id','desc')->paginate($request->input('limit'));
+    public function newbooks(Request $request)
+    {
+        $book = Book::orderBy('id', 'desc')->paginate($request->input('limit'));
         return response([
-            'message'=>"all new books",
-            'data'=>UserBooksResource::collection($book),
-            'total'=>$book->total()
+            'message' => "all new books",
+            'data' => UserBooksResource::collection($book),
+            'total' => $book->total()
         ]);
     }
 
     public function index(Request $request)
     {
-        $book=Book::paginate($request->input('limit'));
+        $book = Book::paginate($request->input('limit'));
         return response([
-            'message'=>"all books",
-            'data'=>UserBooksResource::collection($book),
-            'total'=>$book->total()
+            'message' => "all books",
+            'data' => UserBooksResource::collection($book),
+            'total' => $book->total()
         ]);
     }
 
     public function index_user(Request $request)
     {
-        $book=Book::paginate($request->input('limit'));
+        $book = Book::paginate($request->input('limit'));
         return response([
-            'message'=>"all books",
-            'data'=>UserBooksResource::collection($book),
-            'total'=>$book->total()
+            'message' => "all books",
+            'data' => UserBooksResource::collection($book),
+            'total' => $book->total()
         ]);
     }
-    public function recommendation_user(Request $request){
-        $book=Book::orderBy('rating','desc')->paginate($request->input('limit'));
+    public function recommendation_user(Request $request)
+    {
+        $book = Book::orderBy('rating', 'desc')->paginate($request->input('limit'));
         return response([
-            'message'=>"all recommendation books",
-            'data'=>UserBooksResource::collection($book),
-            'total'=>$book->total()
+            'message' => "all recommendation books",
+            'data' => UserBooksResource::collection($book),
+            'total' => $book->total()
         ]);
     }
 
-    public function popular_user(Request $request){
-        $book=Book::orderBy('click','desc')->paginate($request->input('limit'));
+    public function popular_user(Request $request)
+    {
+        $book = Book::orderBy('click', 'desc')->paginate($request->input('limit'));
         return response([
-            'message'=>"all popular books",
-            'data'=>UserBooksResource::collection($book),
-            'total'=>$book->total()
+            'message' => "all popular books",
+            'data' => UserBooksResource::collection($book),
+            'total' => $book->total()
         ]);
     }
-    public function newbooks_user(Request $request){
-        $book=Book::orderBy('id','desc')->paginate($request->input('limit'));
+    public function newbooks_user(Request $request)
+    {
+        $book = Book::orderBy('id', 'desc')->paginate($request->input('limit'));
         return response([
-            'message'=>"all new books",
-            'data'=>UserBooksResource::collection($book),
-            'total'=>$book->total()
+            'message' => "all new books",
+            'data' => UserBooksResource::collection($book),
+            'total' => $book->total()
         ]);
     }
-  
+
     public function show_user($id)
     {
         $user = auth()->user();
-        $userbook = Order::where('book_id', $id)->where('user_id', $user->id)->where('status',true)->first();
+        $userbook = Order::where('book_id', $id)->where('user_id', $user->id)->where('status', true)->first();
         $book = Book::find($id);
         if (isset($book)) {
             $book->update([
@@ -127,157 +134,151 @@ class BookController extends Controller
         }
     }
 
-    public function newbooks_admin(Request $request){
-        $book=Book::orderBy('id','desc')->paginate($request->input('limit'));
+    public function newbooks_admin(Request $request)
+    {
+        $book = Book::orderBy('id', 'desc')->paginate($request->input('limit'));
         return response([
-            'message'=>"all new books",
-            'data'=>AdminBookResource::collection($book),
-            'total'=>$book->total()
+            'message' => "all new books",
+            'data' => AdminBookResource::collection($book),
+            'total' => $book->total()
         ]);
     }
 
     public function show_admin($id)
     {
-        $book=Book::find($id);
-        if(isset($book))
-        {
+        $book = Book::find($id);
+        if (isset($book)) {
             return response([
-                'message'=>'one category',
-                'data'=>new AdminOneBookResource($book)
+                'message' => 'one category',
+                'data' => new AdminOneBookResource($book)
             ]);
-        }
-        else {
+        } else {
             return response([
-                'message'=>'id not found'
-            ],404);
+                'message' => 'id not found'
+            ], 404);
         }
     }
 
-    public function book_rating_click($id){
-        $book=Book::find($id);
-        if(isset($book)){
-            $users=Review::where('book_id',$id)->get()->count();
+    public function book_rating_click($id)
+    {
+        $book = Book::find($id);
+        if (isset($book)) {
+            $users = Review::where('book_id', $id)->get()->count();
             return response([
-                'message'=>'rating click',
-                'data'=>[
-                    'comments_count'=>$users,
-                    'rating'=>$book->rating,
-                    'clicks'=>$book->click
+                'message' => 'rating click',
+                'data' => [
+                    'comments_count' => $users,
+                    'rating' => $book->rating,
+                    'clicks' => $book->click
                 ]
             ]);
-        }else {
+        } else {
             return response([
-                'message'=>'id not found'
-            ],404);
+                'message' => 'id not found'
+            ], 404);
         }
     }
 
 
-   
+
     public function store(StoreBookRequest $request)
     {
-        $b=false;
+        $b = false;
         if ($file = $request->file('image')) {
             $name = time() . $file->getClientOriginalName();
-            $request->image->move(public_path('/images'),$name);
-            $b=true; 
+            $request->image->move(public_path('/images'), $name);
+            $b = true;
         }
-        $result=$request->validated();
-        $book=Book::create($result);
-        $book=Book::find($book->id);
-        if($b)
+        $result = $request->validated();
+        $book = Book::create($result);
+        $book = Book::find($book->id);
+        if ($b)
             $book->photo()->create([
-                'file'=>$name,
+                'file' => $name,
             ]);
         $book->categories()->sync([$request['categories_id']]);
-        
+
         return response([
-            'message'=>"created book",
-            'data'=>new OneBookResource($book)
+            'message' => "created book",
+            'data' => new OneBookResource($book)
         ], 201);
     }
 
-   
+
     public function show($id)
     {
-        $book=Book::find($id);
-        if(isset($book))
-        {
+        $book = Book::find($id);
+        if (isset($book)) {
             $book->update([
-                'click'=>$book->click+1
+                'click' => $book->click + 1
             ]);
             return response([
-                'message'=>'one category',
-                'data'=>new OneBookResource($book)
+                'message' => 'one category',
+                'data' => new OneBookResource($book)
             ]);
-        }
-        else {
+        } else {
             return response([
-                'message'=>'id not found'
-            ],404);
+                'message' => 'id not found'
+            ], 404);
         }
     }
 
     public function update(UpdateBookRequest $request, $id)
     {
-        $book=Book::find($id);
+        $book = Book::find($id);
         if ($book) {
-            $b=false;
+            $result = $request->validated();
+            $b = false;
             if ($file = $request->file('image')) {
                 $name = time() . $file->getClientOriginalName();
                 // $file->storeAs('public/images/', $name);
-                unlink('images/'.$book->photo->file);
-                $request->image->move(public_path('/images'),$name);
+                unlink('images/' . $book->photo->file);
 
-                $b=true;
-            }
-            $result=$request->validated();
-            if(isset($book->photo) && $b)
-            {
-                // Storage::delete("public/images/".$book->photo->file);
-                
-                $book->photo()->delete();
+                if (isset($book->photo)) {
+                    // Storage::delete("public/images/".$book->photo->file);
+
+                    $book->photo()->delete();
+                }
+                $request->image->move(public_path('/images'), $name);
                 $book->photo()->create([
-                    'file'=>$name,
+                    'file' => $name,
                 ]);
             }
-            
+
             $book->update($result);
-            if(isset($request->categories_id))
+            if (isset($request->categories_id))
                 $book->categories()->sync([$request->categories_id]);
             return response([
-                'message'=>'book updated succsesfull',
-                'data'=>new OneBookResource($book)
+                'message' => 'book updated succsesfull',
+                'data' => new OneBookResource($book)
             ]);
         } else {
             return response([
-               'message'=>'id not found'
-            ],404);
+                'message' => 'id not found'
+            ], 404);
         }
     }
 
-   
+
     public function destroy($id)
     {
-        $book=Book::find($id);
-        if(isset($book))
-        {
-            CategoryBook::where('book_id',$id)->delete();
+        $book = Book::find($id);
+        if (isset($book)) {
+            CategoryBook::where('book_id', $id)->delete();
             // Storage::delete("public/images/".$book->photo->file);
             // if(isset($book->photo->file)){
             //     $book->photo->delete();
             //     unlink('images/'.$book->photo->file);
             // }
-                $book->delete();
-            
+            $book->delete();
+
             return response([
-               'message'=>'book deleted'
+                'message' => 'book deleted'
             ]);
-        }
-        else {
+        } else {
             return response([
-               'message'=>'id not found'
-            ],404);
+                'message' => 'id not found'
+            ], 404);
         }
     }
 }
